10dc6c6425a39fc9d6486a929190b10689f8149f
Erick Garcia <erickarmandogarcia@hotmail.com>
2015-04-18 12:11:42 -0500

    Add basic routing to serve index.html and app.js files.

--- server.js
+++ server.js
@@ -12,21 +12,52 @@ server.on('request',onRequest)
 server.on('listening',onListening)
 
 function onRequest(req,res){
-    let index=path.join(__dirname,'public','index.html')
+  let uri =req.url
+  
+  if(uri.startsWith('/index') || uri ==='/'){
+    return serverIndex(res)
+  }
+  
+  if(uri ==='/app.js'){
+    return serverApp(res)
+  }
 
-    //set the content type
-    res.setHeader('Content-Type','text/html')
+  res.statusCode=404
+  res.end(`404 not found: ${uri}`)
+}
 
-    //define a read stream
-    let rs=fs.createReadStream(index)
+function serverIndex(res){
+  let index=path.join(__dirname,'public','index.html')
+  //define a read stream
+  let rs=fs.createReadStream(index)
+  //set the header's content type
+  res.setHeader('Content-Type','text/html')
 
-    //pass the read stream through the response stream
-    rs.pipe(res)
+  //pass the read stream through the response stream
+  rs.pipe(res)
 
-    //define and error listener on the read stream
-    rs.on('error',function(err){
-        res.end(err.message)
-    })
+  //define and error listener on the read stream
+  rs.on('error',function(err){
+      res.end(err.message)
+  })
+}
+
+function serverApp(res){
+
+  let app=path.join(__dirname,'public','app.js')
+  //define a read stream
+  let rs=fs.createReadStream(app)
+
+  //set the content type
+  res.setHeader('Content-Type','text/html')
+
+  //pass the read stream through the response stream
+  rs.pipe(res)
+
+  //define and error listener on the read stream
+  rs.on('error',function(err){
+      res.end(err.message)
+  })
 }
 
 function onListening(){
